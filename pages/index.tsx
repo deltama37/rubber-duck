26f4f0d44d1b8175f2bac6bede8630fd4e0a1b81
ryota misumi <3delta7@gmail.com>
2021-09-11 11:06:09 +0900

    add chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
 const Home = () => {
-  return <></>;
+  return (
+    <header>
+      <Button>Toggle</Button>
+    </header>
+  );
 };
 
 export default Home;
